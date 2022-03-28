9f648b1cffde2521905172950a5371e33fa355fb
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-03-28 23:35:57 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC29K-2cd_apd9G2sAQw6Q34DJYEtVhwK0",
+  authDomain: "joinantest.firebaseapp.com",
+  projectId: "joinantest",
+  storageBucket: "joinantest.appspot.com",
+  messagingSenderId: "727341133911",
+  appId: "1:727341133911:web:b36ae167f1605ecbc17312",
+  measurementId: "G-3H6TK3PP70",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form-group
+          id="username-input"
+          label="Username :"
+          label-for="username-input"
+        >
+          <b-form-input
+            id="username-input"
+            type="text"
+            placeholder="Username..."
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="password-input"
+          label="Password :"
+          label-for="password-input"
+        >
+          <b-form-input
+            id="password-input"
+            type="password"
+            placeholder="password..."
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button variant="primary">
+          <span>Login</span>
+        </b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
